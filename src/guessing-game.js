03c193f778af7eaa9f4dacea9eb945f06ad87a28
40iq <maksPTD6@mail.ru>
2023-09-04 21:52:53 +0300

    feat: done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num = Math.round((this.max - this.min) / 2) + this.min;
+        return this.num;
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
+        this.min = this.num;
     }
 }
 
